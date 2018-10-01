68fc01008697adcc2ee766ed6d7a265a0a07c5e3
George  Pianka <gpianka1@gmail.com>
2018-10-01 04:14:47 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,16 @@
 CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  
+
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY, 
+
+);
+
+CREATE TABLE pledges (
   id INTEGER PRIMARY KEY, 
 
 );
